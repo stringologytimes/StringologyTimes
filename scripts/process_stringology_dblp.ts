7c2e2a268cbb97d4a2eb1c15fd46f2d1e8bb9d93
takaaki.nishimoto <takaaki.nishimoto@riken.jp>
2022-05-02 00:09:20 +0900

    minor fix

--- scripts/process_stringology_dblp.ts
+++ scripts/process_stringology_dblp.ts
@@ -85,7 +85,7 @@ function createMD(_papers: DBLPElement[], year: number) : string[] {
 function createArxivYearMonthMD(_papers: DBLPElement[], year : number) : string[] {
     const lines: string[] = new Array();
     for(var i = 12; i >= 1 ;i--){
-        const list = _papers.filter((v) => v.mdate.getFullYear() == year && (v.mdate.getMonth()+1) == i);
+        const list = _papers.filter((v) => v.year == year && (v.mdate.getMonth()+1) == i);
         if(list.length > 0){
             lines.push(`### ${year}/${i}  `);
             list.forEach((v, x) =>{
@@ -103,13 +103,13 @@ function createArxivMD(_papers: DBLPElement[]) : string[] {
     const lines: string[] = new Array();
     lines.push(`# arXiv Papers  `)
     
-    const yearList : number[] = [...new Set(_papers.map((v) => v.mdate.getFullYear()))];
+    const yearList : number[] = [...new Set(_papers.map((v) => v.year ))];
     yearList.sort((a, b) =>{
         return a > b ? -1 : 1;
     })
     yearList.forEach((year) =>{
         lines.push(`## ${year}  `)
-        const spapers = _papers.filter((v) => v.mdate.getFullYear() == year);
+        const spapers = _papers.filter((v) => v.year == year);
         createArxivYearMonthMD(spapers, year).forEach((v) => lines.push(v));
         lines.push(`  `);
     })
